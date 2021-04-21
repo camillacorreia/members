3ebf392c56e3bf4404b5f5700cd548db7ec42989
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-21 17:27:45 -0300

    refactor: remove storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/global'
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Membros - TITAN</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#F2A900" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Mapa interativo dos membros da TITAN"
         />
       </Head>
-      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
