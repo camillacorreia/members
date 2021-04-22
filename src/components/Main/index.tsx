fa6cc34aa53802008296388eb3f0304539813f28
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-21 21:33:28 -0300

    feat: add leaflet and create Map Component

--- src/components/Main/index.tsx
+++
@@ -1,3 +0,0 @@
-const Main = () => <h1>Members</h1>
-
-export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,12 @@ function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Mapa interativo dos membros da TITAN"
         />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
