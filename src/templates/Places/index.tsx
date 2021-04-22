69401f796935e8123650b8832e967a648658d88a
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-22 08:55:19 -0300

    feat: add routes to markers on map

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image'
+
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import * as S from './styles'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description?: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}
